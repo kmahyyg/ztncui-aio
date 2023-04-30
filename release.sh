5e7219e897c83e5661398d7864796235cb57a239
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-04-30 19:12:10 +0800

    add comment for original release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#
 # Derived from https://medium.com/travis-on-docker/how-to-version-your-docker-images-1d5c577ebf54
+#
+# This script has only been used by docker.io/key-networks/ztncui
+# 
+# In kmahyyg/ztncui-aio, the publishing process is done by github-action
+#
 
 set -ex
 
