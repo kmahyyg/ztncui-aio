10bab81c1af677a6069d7041b85460404f52549a
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-05-01 11:37:30 +0800

    remove all github action and refine supported arch

--- build-ztncui.sh
+++ build-ztncui.sh
@@ -8,16 +8,16 @@ npm install
 MACHINE_ARCHITECTURE=$(uname -m)
 export NODEJS_PACK_ARCH=""
 case ${MACHINE_ARCHITECTURE} in 
-    "arm")
+    "arm"|"armv7l"|"armv7"|"armhf")
         # armv6 is too old, and unsupported by pkg, and should be deprecated, so we won't support
         # armv7 is unsupported by pkg, you could try build yourself, but no guarantee here
         echo "Upstream unsupported architecture: ${MACHINE_ARCHITECTURE}. Treat as armv7. But will still try to build."
         NODEJS_PACK_ARCH="armv7"
         ;;
-    "aarch64")
+    "aarch64"|"arm64")
         NODEJS_PACK_ARCH="arm64"
         ;;
-    "x86_64")
+    "x86_64"|"amd64")
         NODEJS_PACK_ARCH="x64"
         ;;
     *)
