fc3dcd6e7e81b7b264b3d6ce2a42c3595b8af351
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2024-05-03 00:33:39 +0800

    update fileserv to allow listen locally

--- fileserv/main.go
+++ fileserv/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	var fs http.FileSystem = http.Dir("/opt/key-networks/ztncui/etc/httpfs")
 	var fsHandler = http.FileServer(fs)
-	log.Fatal(http.ListenAndServe(":3180", fsHandler))
+	var listenGlobal = os.Getenv("PLANET_RETR_PUBLIC")
+	if listenGlobal != "" {
+		log.Fatal(http.ListenAndServe(":3180", fsHandler))
+	} else {
+		log.Fatal(http.ListenAndServe("127.0.0.1:3180", fsHandler))
+	}
 }
